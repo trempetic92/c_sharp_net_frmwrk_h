e10870815f96adbb7f02e44fe5c0c4cac0780c5c
Luka Trempetić <trempetic92@gmail.com>
2024-01-29 20:01:28 +0100

    update zad7

--- SedmiZadatak.cs
+++ SedmiZadatak.cs
@@ -18,6 +18,7 @@ namespace c_sharp_net_frmwrk_h
 
             int x = 1;
             int y = 3;
+            Console.WriteLine("Usporedba vrijednosti...");
 
             Console.WriteLine(x > y);
             Console.WriteLine(x < y);
@@ -27,6 +28,26 @@ namespace c_sharp_net_frmwrk_h
             Console.WriteLine(x > y);
             Console.WriteLine(x);
             Console.WriteLine(y);
+
+            Console.WriteLine("=================\n");
+
+            Console.WriteLine("Tablica istinitosti za logičke operatore...");
+            Console.WriteLine("Negacija\n!");
+            Console.WriteLine(!true);  //false
+            Console.WriteLine(!false); //true
+            Console.WriteLine();
+            Console.WriteLine("Logički I\t&&");
+            Console.WriteLine(true && true);   //1
+            Console.WriteLine(true && false);  //0
+            Console.WriteLine(false && true);  //0
+            Console.WriteLine(false && false); //0
+
+            Console.WriteLine();
+            Console.WriteLine("Logički ILI\t||");
+            Console.WriteLine(true || true);   //1
+            Console.WriteLine(true || false);  //1
+            Console.WriteLine(false || true);  //1
+            Console.WriteLine(false || false); //0
         }
     }
 }
--- Zadaca3.cs
+++ Zadaca3.cs
@@ -12,6 +12,7 @@ namespace c_sharp_net_frmwrk_h
         {
             long velikiBroj = 123456789012345;
             Console.WriteLine((int)velikiBroj);
+            Console.WriteLine("If the long value is outside the int range, the conversion\n effectively takes the lowest 32 bits of the long value and interprets that as an int.");
         }
     }
 }
